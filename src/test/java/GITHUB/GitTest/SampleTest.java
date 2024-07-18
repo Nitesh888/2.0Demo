536b0d52e90e1e970aaafcc8d34ffe26fb7090d1
Nitesh888 <m.nani.yuvi@gmail.com>
2024-07-19 03:50:54 +0530

    anger

---
+++ src/test/java/GITHUB/GitTest/SampleTest.java
@@ -0,0 +1,18 @@
+package GITHUB.GitTest;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	  System.out.println("Hello");
+	  System.out.println("goodBye");
+	  System.out.println("Hi");
+	  System.out.println("Namaste");
+	  System.out.println("see you");
+	  System.out.println("TATA");
+	  System.out.println("NAMASTE");
+	  System.out.println("vanakam");
+	}
+
+}
